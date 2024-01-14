6486c36fe9d7aa22b49236ed3b83152b14290f8b
sudhdkso <waga1009@gmail.com>
2024-01-15 02:49:39 +0900

    test: sonar cube pr test

--- src/main/java/com/project/openmarket/test.java
+++ src/main/java/com/project/openmarket/test.java
@@ -1,2 +1,10 @@
-package com.project.openmarket;public class test {
+package com.project.openmarket;
+
+public class test {
+	int id = 0;
+
+	public void test() {
+
+		for(int i=1;i<10;i++){id++;};
+	}
 }
