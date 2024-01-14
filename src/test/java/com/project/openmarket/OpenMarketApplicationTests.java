885a972284ff6638e055b84c8765faf2eb04535d
sudhdkso <waga1009@gmail.com>
2024-01-15 01:07:53 +0900

    chore: initial commit

---
+++ src/test/java/com/project/openmarket/OpenMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.openmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
