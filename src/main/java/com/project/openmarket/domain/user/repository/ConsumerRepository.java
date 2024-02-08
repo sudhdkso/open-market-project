17453d969986755a507881dd7f50306712e1a960
sudhdkso <waga1009@gmail.com>
2024-02-08 14:14:40 +0900

    ✨feat: 고객 등록 시 이메일 중복 체크 기능 추가

--- src/main/java/com/project/openmarket/domain/user/repository/ConsumerRepository.java
+++ src/main/java/com/project/openmarket/domain/user/repository/ConsumerRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.openmarket.domain.user.entity.Consumer;
 
 public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
+	public boolean existsByEmail(String email);
+
 }
--- src/main/java/com/project/openmarket/domain/user/service/ConsumerService.java
+++ src/main/java/com/project/openmarket/domain/user/service/ConsumerService.java
@@ -3,10 +3,10 @@ package com.project.openmarket.domain.user.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.project.openmarket.domain.user.entity.Consumer;
-import com.project.openmarket.domain.user.repository.ConsumerRepository;
 import com.project.openmarket.domain.user.dto.reposne.UserCreateResponseDto;
 import com.project.openmarket.domain.user.dto.request.ConsumerCreateReqestDto;
+import com.project.openmarket.domain.user.entity.Consumer;
+import com.project.openmarket.domain.user.repository.ConsumerRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,8 +17,15 @@ public class ConsumerService {
 
 	@Transactional
 	public UserCreateResponseDto saveConumser(ConsumerCreateReqestDto request){
-		Consumer consumer = consumerRepository.save(request.toEntity());
-		return UserCreateResponseDto.of(consumer);
+		Consumer consumer = request.toEntity();
+		duplicatedEmail(consumer.getEmail());
+		return UserCreateResponseDto.of(consumerRepository.save(consumer));
 	}
 
+	private void duplicatedEmail(final String email){
+		if(consumerRepository.existsByEmail(email)){
+			throw new IllegalArgumentException();
+			// customException으로 변경
+		}
+	}
 }
