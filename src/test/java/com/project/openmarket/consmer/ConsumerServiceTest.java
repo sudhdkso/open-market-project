854f5c4dd636f39063087f373be29fc1f80a2ac0
sudhdkso <waga1009@gmail.com>
2024-02-08 15:46:17 +0900

    ✅test: 테스트 코드 순서 수정

--- src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
+++ src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
@@ -3,7 +3,10 @@ package com.project.openmarket.consmer;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,11 +15,13 @@ import com.project.openmarket.domain.user.dto.request.ConsumerCreateReqestDto;
 import com.project.openmarket.domain.user.service.ConsumerService;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ConsumerServiceTest {
 	@Autowired
 	private ConsumerService consumerService;
 
 	@Test
+	@Order(1)
 	@DisplayName("고객등록_테스트")
 	void 고객_등록_테스트(){
 		//given
@@ -31,12 +36,12 @@ public class ConsumerServiceTest {
 	}
 
 	@Test
+	@Order(2)
 	@DisplayName("고객등록_이메일중복_테스트")
 	void 이메일_중복_테스트(){
 		ConsumerCreateReqestDto request = new ConsumerCreateReqestDto("asdf3@example.com","dd","010-0000-0000","1234","");
 
 		// when
-		consumerService.saveConumser(request);
 		Throwable thrown = catchThrowable(() -> consumerService.saveConumser(request));
 
 		//then
