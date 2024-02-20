cc60456cd5c003b888515a120ea463ca3f7e2198
sudhdkso <waga1009@gmail.com>
2024-02-20 14:48:34 +0900

    ✅test: 로그인 서비스 테스트 코드 추가

--- src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
+++ src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.project.openmarket.domain.user.dto.reposne.UserCreateResponseDto;
 import com.project.openmarket.domain.user.dto.request.ConsumerCreateReqestDto;
+import com.project.openmarket.domain.user.dto.request.ConsumerLoginRequestDto;
 import com.project.openmarket.domain.user.service.ConsumerService;
 
 @SpringBootTest
@@ -23,12 +23,12 @@ class ConsumerServiceTest {
 	@Test
 	@Order(1)
 	@DisplayName("고객등록_테스트")
-	void 고객_등록_테스트(){
+	void signupConsumer(){
 		//given
-		ConsumerCreateReqestDto request = new ConsumerCreateReqestDto("asdf3@example.com","dd","010-0000-0000","1234","");
+		final var request = ConsumerStep.고객생성요청_생성();
 
-		//when
-		UserCreateResponseDto response = consumerService.saveConumser(request);
+ 		//when
+		final var response = consumerService.saveConumser(request);
 
 		//then
 		assertThat(request.email())
@@ -38,11 +38,55 @@ class ConsumerServiceTest {
 	@Test
 	@Order(2)
 	@DisplayName("고객등록_이메일중복_테스트")
-	void 이메일_중복_테스트(){
-		ConsumerCreateReqestDto request = new ConsumerCreateReqestDto("asdf3@example.com","dd","010-0000-0000","1234","");
+	void sinupByDuplicatedEmail(){
+		final var request = new ConsumerCreateReqestDto("asdf@example.com","dd","010-0000-0000","1234","");
 
 		// when
 		Throwable thrown = catchThrowable(() -> consumerService.saveConumser(request));
+
+		//then
+		assertThat(thrown)
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@Order(3)
+	@DisplayName("고객_로그인_성공_테스트")
+	void consumerLogin(){
+		//given
+		final var request = ConsumerStep.로그인요청_생성();
+
+		//when
+		final var response = consumerService.login(request);
+
+		//then
+		assertThat(response.email())
+			.isEqualTo(request.email());
+	}
+
+	@Test
+	@Order(4)
+	@DisplayName("고객_없는_이메일_로그인_실패_테스트")
+	void loginByWrongEmail(){
+		//given
+		final var request = new ConsumerLoginRequestDto("test@example.com", "1234");
+
+		// when
+		Throwable thrown = catchThrowable(() -> consumerService.login(request));
+
+		//then
+		assertThat(thrown)
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("고객_로그인_틀린_패스워드_테스트")
+	void loginByWrongPassword(){
+		//given
+		final var request = new ConsumerLoginRequestDto("asdf3@example.com", "12345");
+
+		// when
+		Throwable thrown = catchThrowable(() -> consumerService.login(request));
 
 		//then
 		assertThat(thrown)
