00d0a75d5dfea759bc4f9cabc1ceae7e393244b9
sudhdkso <waga1009@gmail.com>
2024-02-20 16:12:11 +0900

    ✅test: 테스트 코드 repository mock으로 수정

--- src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
+++ src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
@@ -1,33 +1,41 @@
 package com.project.openmarket.consmer;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.project.openmarket.domain.user.dto.request.ConsumerCreateReqestDto;
 import com.project.openmarket.domain.user.dto.request.ConsumerLoginRequestDto;
+import com.project.openmarket.domain.user.entity.Consumer;
+import com.project.openmarket.domain.user.repository.ConsumerRepository;
 import com.project.openmarket.domain.user.service.ConsumerService;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ConsumerServiceTest {
-	@Autowired
+	@InjectMocks
 	private ConsumerService consumerService;
-
+	@Mock
+	private ConsumerRepository consumerRepository;
 	@Test
 	@Order(1)
 	@DisplayName("고객등록_테스트")
 	void signupConsumer(){
 		//given
-		final var request = ConsumerStep.고객생성요청_생성();
-
+		final var request = createConsumer();
  		//when
+		when(consumerRepository.save(any(Consumer.class))).thenReturn(request.toEntity());
 		final var response = consumerService.saveConumser(request);
 
 		//then
@@ -39,9 +47,10 @@ class ConsumerServiceTest {
 	@Order(2)
 	@DisplayName("고객등록_이메일중복_테스트")
 	void sinupByDuplicatedEmail(){
-		final var request = new ConsumerCreateReqestDto("asdf@example.com","dd","010-0000-0000","1234","");
+		final var request = new ConsumerCreateReqestDto("asdf3@example.com","dd","010-0000-0000","1234","");
 
 		// when
+		when(consumerRepository.existsByEmail(request.email())).thenReturn(true);
 		Throwable thrown = catchThrowable(() -> consumerService.saveConumser(request));
 
 		//then
@@ -80,9 +89,10 @@ class ConsumerServiceTest {
 	@DisplayName("고객_로그인_성공_테스트")
 	void consumerLogin(){
 		//given
-		final var request = ConsumerStep.로그인요청_생성();
+		final var request = createLogin();
 
 		//when
+		when(consumerRepository.findByEmail(request.email())).thenReturn(Optional.of(createConsumer().toEntity()));
 		final var response = consumerService.login(request);
 
 		//then
@@ -98,6 +108,7 @@ class ConsumerServiceTest {
 		final var request = new ConsumerLoginRequestDto("test@example.com", "1234");
 
 		// when
+		when(consumerRepository.findByEmail(request.email())).thenReturn(Optional.empty());
 		Throwable thrown = catchThrowable(() -> consumerService.login(request));
 
 		//then
@@ -118,5 +129,22 @@ class ConsumerServiceTest {
 		assertThat(thrown)
 			.isInstanceOf(IllegalArgumentException.class);
 
+	}
+	
+	private ConsumerCreateReqestDto createConsumer(){
+		String email = "asdf3@example.com";
+		String name = "김하얀";
+		String phoneNumber = "010-0000-0000";
+		String password = "1234";
+		String address = "어디지";
+		
+		return new ConsumerCreateReqestDto(email, name, phoneNumber, password, address);
+	}
+
+	private ConsumerLoginRequestDto createLogin(){
+		String email = "asdf3@example.com";
+		String password = "1234";
+
+		return new ConsumerLoginRequestDto(email, password);
 	}
 }
