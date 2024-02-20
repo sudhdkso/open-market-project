f881cdf920e25db61ba70afcdc237c3a16369ce1
sudhdkso <waga1009@gmail.com>
2024-02-20 14:48:00 +0900

    ✅test: 로그인 api 테스트 코드 추가

--- src/test/java/com/project/openmarket/consmer/ConsumerApiTest.java
+++ src/test/java/com/project/openmarket/consmer/ConsumerApiTest.java
@@ -12,9 +12,20 @@ class ConsumerApiTest extends ApiTest {
 
 	@Test
 	@DisplayName("고객등록요청_api_테스트")
-	void 고객등록(){
+	void signupApi(){
 		final var response = ConsumerStep.고객생성요청(ConsumerStep.고객생성요청_생성());
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+	}
+
+	@Test
+	@DisplayName("로그인_api_테스트")
+	void loginApi(){
+
+		ConsumerStep.고객생성요청(ConsumerStep.고객생성요청_생성());
+
+		final var response = ConsumerStep.로그인요청(ConsumerStep.로그인요청_생성());
+	    assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
 	}
 
 }
