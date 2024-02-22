f2b83fea28f9ae15a6a76d0131cc72f895444696
sudhdkso <waga1009@gmail.com>
2024-02-22 14:04:02 +0900

    ✅test: 통합한 로그인 기능 api 테스트 코드 추가 및 기존 테스트 코드 displayName 수정 및 함수명 변경에 따른 수정

--- src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
+++ src/test/java/com/project/openmarket/consmer/ConsumerServiceTest.java
@@ -16,7 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.project.openmarket.domain.user.dto.request.ConsumerCreateReqestDto;
-import com.project.openmarket.domain.user.dto.request.ConsumerLoginRequestDto;
+import com.project.openmarket.domain.user.dto.request.LoginRequestDto;
 import com.project.openmarket.domain.user.entity.Consumer;
 import com.project.openmarket.domain.user.repository.ConsumerRepository;
 import com.project.openmarket.domain.user.service.ConsumerService;
@@ -28,6 +28,7 @@ class ConsumerServiceTest {
 	private ConsumerService consumerService;
 	@Mock
 	private ConsumerRepository consumerRepository;
+
 	@Test
 	@Order(1)
 	@DisplayName("고객등록_테스트")
@@ -36,7 +37,7 @@ class ConsumerServiceTest {
 		final var request = createConsumer();
  		//when
 		when(consumerRepository.save(any(Consumer.class))).thenReturn(request.toEntity());
-		final var response = consumerService.saveConumser(request);
+		final var response = consumerService.signup(request);
 
 		//then
 		assertThat(request.email())
@@ -45,13 +46,13 @@ class ConsumerServiceTest {
 
 	@Test
 	@Order(2)
-	@DisplayName("고객등록_이메일중복_테스트")
+	@DisplayName("고객등록_이메일중복_테스트시_예외가_발생한다")
 	void sinupByDuplicatedEmail(){
 		final var request = new ConsumerCreateReqestDto("asdf3@example.com","dd","010-0000-0000","1234","");
 
 		// when
 		when(consumerRepository.existsByEmail(request.email())).thenReturn(true);
-		Throwable thrown = catchThrowable(() -> consumerService.saveConumser(request));
+		Throwable thrown = catchThrowable(() -> consumerService.signup(request));
 
 		//then
 		assertThat(thrown)
@@ -60,7 +61,7 @@ class ConsumerServiceTest {
 
 	@Test
 	@Order(2)
-	@DisplayName("고객등록_이메일_없이_테스트")
+	@DisplayName("고객등록_이메일_없이_테스트시_예외가_발생한다")
 	void sinupByEmptyEmail(){
 		//given
 		String email = "";
@@ -74,7 +75,7 @@ class ConsumerServiceTest {
 
 	@Test
 	@Order(2)
-	@DisplayName("고객등록_모두_빈값_테스트")
+	@DisplayName("고객등록_모두_빈값_테스트시_예외가_발생한다")
 	void sinupByAllEmptyInfo(){
 		// when
 		Throwable thrown = catchThrowable(() -> new ConsumerCreateReqestDto("","","","",""));
@@ -102,13 +103,13 @@ class ConsumerServiceTest {
 
 	@Test
 	@Order(4)
-	@DisplayName("고객_없는_이메일_로그인_실패_테스트")
+	@DisplayName("고객_없는_이메일_로그인_테스트시_예외가_발생한다")
 	void loginByWrongEmail(){
 		//given
-		final var request = new ConsumerLoginRequestDto("test@example.com", "1234");
+		final var request = new LoginRequestDto("test@example.com", "1234");
 
 		// when
-		when(consumerRepository.findByEmail(request.email())).thenReturn(Optional.empty());
+		when(consumerRepository.findByEmail(any(String.class))).thenReturn(Optional.empty());
 		Throwable thrown = catchThrowable(() -> consumerService.login(request));
 
 		//then
@@ -117,10 +118,10 @@ class ConsumerServiceTest {
 	}
 
 	@Test
-	@DisplayName("고객_로그인_틀린_패스워드_테스트")
+	@DisplayName("고객_로그인_틀린_패스워드_테스트시_예외가_발생한다")
 	void loginByWrongPassword(){
 		//given
-		final var request = new ConsumerLoginRequestDto("asdf3@example.com", "12345");
+		final var request = new LoginRequestDto("asdf3@example.com", "12345");
 
 		// when
 		Throwable thrown = catchThrowable(() -> consumerService.login(request));
@@ -141,10 +142,10 @@ class ConsumerServiceTest {
 		return new ConsumerCreateReqestDto(email, name, phoneNumber, password, address);
 	}
 
-	private ConsumerLoginRequestDto createLogin(){
+	private LoginRequestDto createLogin(){
 		String email = "asdf3@example.com";
 		String password = "1234";
 
-		return new ConsumerLoginRequestDto(email, password);
+		return new LoginRequestDto(email, password);
 	}
 }
