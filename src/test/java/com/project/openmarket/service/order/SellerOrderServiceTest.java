6bf13895d1fc7b7256df9ab4e2eb599cbc3a6ed3
sudhdkso <waga1009@gmail.com>
2024-10-08 16:05:29 +0900

    ✅test: 판매자 주문 조회에 대한 테스트 코드 작성

--- src/test/java/com/project/openmarket/service/order/SellerOrderServiceTest.java
+++ src/test/java/com/project/openmarket/service/order/SellerOrderServiceTest.java
@@ -2,19 +2,28 @@ package com.project.openmarket.service.order;
 
 import static com.project.openmarket.domain.order.entity.eums.OrderStatus.*;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import com.project.openmarket.domain.order.dto.response.OrderResponseDto;
+import com.project.openmarket.domain.order.entity.Amount;
 import com.project.openmarket.domain.order.entity.Order;
 import com.project.openmarket.domain.order.service.OrderService;
 import com.project.openmarket.domain.order.service.SellerOrderService;
+import com.project.openmarket.domain.product.dto.request.ProductRequestDto;
 import com.project.openmarket.domain.product.entity.Product;
 import com.project.openmarket.domain.product.service.ProductService;
+import com.project.openmarket.domain.user.dto.request.SellerCreateRequestDto;
 import com.project.openmarket.domain.user.entity.Consumer;
+import com.project.openmarket.domain.user.entity.Seller;
 import com.project.openmarket.domain.user.service.ConsumerService;
 import com.project.openmarket.service.ServiceTestMock;
 
@@ -68,5 +77,50 @@ class SellerOrderServiceTest extends ServiceTestMock {
 		then(orderService)
 			.should(times(1))
 			.processOrderCancel(any(Order.class), any(Product.class), any(Consumer.class));
+	}
+
+
+	@Test
+	@DisplayName("판매자 주문이 있는 경우 findOrdersBySellerId는 orderList를 return한다.")
+
+	void findOrdersBySeller_success() {
+		// given
+
+		var sellerReuestDto = new SellerCreateRequestDto("seller@example.com","seller","010-0000-0000","1234");
+		var seller = spy(Seller.of(sellerReuestDto));
+
+		var productRequestDto = new ProductRequestDto("product",1000,2);
+		var product = Product.of(productRequestDto, seller);
+		var order1 = new Order(product, null, new Amount(1000L, 0L), 0, null);
+		var order2 = new Order(product, null, new Amount(0L,1000L), 0, null);
+
+		List<Order> mockOrders = Arrays.asList(order1, order2);
+
+		given(seller.getId()).willReturn(1L);
+		when(orderRepository.findOrdersBySellerId(any())).thenReturn(mockOrders);
+
+		// when
+		List<OrderResponseDto> result = sellerOrderService.findOrdersBySeller(seller);
+
+		// then
+		assertEquals(2, result.size());
+		assertEquals(order1.getSeller(), result.get(0).order().getSeller());
+		assertEquals(order2.getSeller(), result.get(0).order().getSeller());
+
+		verify(orderRepository, times(1)).findOrdersBySellerId(seller.getId());
+	}
+
+
+	@Test
+	@DisplayName("판매자 주문이 없는 경우 findOrdersBySellerId는 빈 리스트를 return한다.")
+	void findOrdersBySeller_noOrdersFound() {
+
+		given(seller.getId()).willReturn(1L);
+		when(orderRepository.findOrdersBySellerId(any())).thenReturn(List.of());
+
+		List<OrderResponseDto> result = sellerOrderService.findOrdersBySeller(seller);
+
+		assertTrue(result.isEmpty());
+		verify(orderRepository, times(1)).findOrdersBySellerId(seller.getId());
 	}
 }
