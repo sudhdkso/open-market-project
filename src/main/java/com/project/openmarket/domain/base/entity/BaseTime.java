803e4a9400bce87b646c8e7923601ba425bc9326
원지윤 <waga1009@gmail.com>
2025-04-02 22:07:11 +0900

    chore: mongodb 설정 추가

--- src/main/java/com/project/openmarket/domain/base/entity/BaseTime.java
+++ src/main/java/com/project/openmarket/domain/base/entity/BaseTime.java
@@ -4,15 +4,12 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@Document
 public abstract class BaseTime {
 	@CreatedDate
 	private LocalDateTime createdAt;
