f7a04e852da0596cec5a3ce022f8c19c2a164187
원지윤 <waga1009@gmail.com>
2025-04-04 16:50:09 +0900

    feat: product update방식 변경 및 paging추가

--- src/main/java/com/project/openmarket/domain/product/controller/ProductController.java
+++ src/main/java/com/project/openmarket/domain/product/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.project.openmarket.domain.product.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -31,48 +30,49 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1")
 public class ProductController {
 	private final ProductService productService;
+
 	@GetMapping("/product/{productId}")
-	public <T>ResponseEntity<ProductResponseDto> getProductById(@PathVariable("productId")String productId) {
+	public <T> ResponseEntity<ProductResponseDto> getProductById(@PathVariable("productId") String productId) {
 		ProductResponseDto responseDto = productService.findById(productId);
 		return ResponseEntity.ok().body(responseDto);
 	}
 
 	@GetMapping("/products")
-	public <T>ResponseEntity<ProductListResponsesDto> getAllProduct(){
+	public <T> ResponseEntity<ProductListResponsesDto> getAllProduct() {
 		ProductListResponsesDto responseDto = productService.findAllProduct();
 		return ResponseEntity.ok().body(responseDto);
 	}
 
 	@GetMapping("/product")
-	public <T>ResponseEntity<List<ProductResponseDto>> searchProduct(
+	public <T> ResponseEntity<Page<ProductResponseDto>> searchProduct(
 		@RequestParam("name") String productName,
 		@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
-	){
-		List<ProductResponseDto> responseDtos = productService.findProductByName(productName, pageable);
+	) {
+		Page<ProductResponseDto> responseDtos = productService.findProductByName(productName, pageable);
 		return ResponseEntity.ok().body(responseDtos);
 	}
 
 	@PostMapping("/seller/product")
-	public <T>ResponseEntity<ProductCreateResponseDto> create(@RequestBody ProductRequestDto requestDto) {
+	public <T> ResponseEntity<ProductCreateResponseDto> create(@RequestBody ProductRequestDto requestDto) {
 		ProductCreateResponseDto responseDto = productService.create(requestDto, SellerThreadLocal.get());
 		//TODO seller용 response 만들기
 		return ResponseEntity.ok().body(responseDto);
 	}
 
-	@PutMapping("/seller/product")
-	public <T>ResponseEntity<ProductResponseDto> update(@RequestBody ProductUpdateReqeustDto requestDto) {
-		ProductResponseDto responseDto = productService.update(requestDto, SellerThreadLocal.get());
+	@PutMapping("/seller/product/{productId}")
+	public <T> ResponseEntity<ProductResponseDto> update(@PathVariable("productId") String productId,
+		@RequestBody ProductUpdateReqeustDto requestDto) {
+		ProductResponseDto responseDto = productService.update(productId, requestDto, SellerThreadLocal.get());
 		return ResponseEntity.ok().body(responseDto);
 	}
 
 	@DeleteMapping("/seller/product/{productId}")
-	public <T>ResponseEntity<?> delete(@PathVariable("productId")Long productId) {
+	public <T> ResponseEntity<?> delete(@PathVariable("productId") String productId) {
 		productService.delete(productId);
 		return ResponseEntity.ok().body("success");
 	}
 
 	//TODO
 	// - 상품 전체 조회
-
 
 }
--- src/main/java/com/project/openmarket/domain/product/service/ProductService.java
+++ src/main/java/com/project/openmarket/domain/product/service/ProductService.java
@@ -3,9 +3,9 @@ package com.project.openmarket.domain.product.service;
 import static com.project.openmarket.global.exception.enums.ExceptionConstants.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,7 +36,7 @@ public class ProductService {
 	 * @param seller 상품을 등록하려는 판매자
 	 * @return 등록된 상품의 정보
 	 */
-	public ProductCreateResponseDto create(ProductRequestDto request, Seller seller){
+	public ProductCreateResponseDto create(ProductRequestDto request, Seller seller) {
 		//상품 이름과 판매자로 일치하는 상품이 있는 경우 예외 발생
 		duplicateProduct(request.name(), seller);
 		Product product = productRepository.save(Product.of(request, seller));
@@ -45,16 +45,17 @@ public class ProductService {
 
 	/**
 	 * 상품을 업데이트하는 메소드
+	 * @param productId 업데이트할 상품의 Id
 	 * @param request 업데이트할 상품의 정보
 	 * @param seller 상품을 업데이터하려는 판매자
 	 * @return 업데이트된 상품의 정보
 	 */
 	@Transactional
-	public ProductResponseDto update(ProductUpdateReqeustDto request, Seller seller){
-		Product product = productRepository.findByIdWithLock(request.id())
+	public ProductResponseDto update(String productId, ProductUpdateReqeustDto request, Seller seller) {
+		Product product = productRepository.findByIdWithLock(new ObjectId(productId))
 			.orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
 		//기존 상품의 이름과 수정하려는 상품의 이름이 다른 경우에만 확인
-		if( request.name() != null && !product.isSameName(request.name())){
+		if (request.name() != null && !product.isSameName(request.name())) {
 			duplicateProduct(request.name(), seller);
 		}
 		product.update(request);
@@ -69,23 +70,22 @@ public class ProductService {
 	 * @param productId 조회하려는 상품의 id
 	 * @return 조회한 상품의 정보
 	 */
-	public ProductResponseDto findById(String productId){
+	public ProductResponseDto findById(String productId) {
 		System.out.println(productId);
 		Product product = getProductById(new ObjectId(productId));
 		return ProductResponseDto.of(product);
 	}
-	
-	public List<Product> findByScoreDesc(String name){
+
+	public List<Product> findByScoreDesc(String name) {
 		return productRepository.findByNameContainsOrderByAvgScoreDesc(name);
 	}
 
-	public List<ProductResponseDto> findProductByName(String name, Pageable pageable){
-		return productRepository.findByName(name)
-			.stream().map(ProductResponseDto::of)
-			.toList();
+	public Page<ProductResponseDto> findProductByName(String name, Pageable pageable) {
+		return productRepository.findByNameContaining(name, pageable)
+			.map(ProductResponseDto::of);
 	}
 
-	public ProductListResponsesDto findAllProduct(){
+	public ProductListResponsesDto findAllProduct() {
 		List<Product> products = productRepository.findAll();
 		return ProductListResponsesDto.of(products);
 	}
@@ -94,37 +94,35 @@ public class ProductService {
 	 * 상품을 삭제하는 메소드
 	 * @param productId 삭제하려는 상품의 id
 	 */
-	public void delete(Long productId){
-		Product product = productRepository.findByIdWithLock(productId)
+	public void delete(String productId) {
+		Product product = productRepository.findByIdWithLock(new ObjectId(productId))
 			.orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
 		productRepository.delete(product);
 	}
 
-	private void duplicateProduct(String name, Seller seller){
-		if(productRepository.existsByNameAndSeller(name, seller)){
+	private void duplicateProduct(String name, Seller seller) {
+		if (productRepository.existsByNameAndSeller(name, seller)) {
 			throw new CustomException(ALREADY_EXISTS_PRODUCT);
 		}
 	}
 
-	public void increaseProductStock(int count, Product product){
+	public void increaseProductStock(int count, Product product) {
 		product.increaseStock(count);
 		productRepository.save(product);
 	}
 
-	public void decreaseProductStock(int count, Product product){
+	public void decreaseProductStock(int count, Product product) {
 		product.decreaseStock(count);
 		productRepository.save(product);
 	}
 
-	public void updateProductAvgScore(double avgScore, Product product){
+	public void updateProductAvgScore(double avgScore, Product product) {
 		product.updateAvgScore(avgScore);
 		productRepository.save(product);
 	}
 
-	public Product getProductById(ObjectId id){
+	public Product getProductById(ObjectId id) {
 		return productRepository.getById(id);
 	}
-
-
 
 }
