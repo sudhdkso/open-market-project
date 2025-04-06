cc3fa23442fbc9d5968fcbfa32f64e7c2c0c2710
원지윤 <waga1009@gmail.com>
2025-04-06 23:28:58 +0900

    test: order의 isReviewed를 업데이트하는 메서드 테스트코드 추가

--- src/test/java/com/project/openmarket/service/order/OrderServiceTests.java
+++ src/test/java/com/project/openmarket/service/order/OrderServiceTests.java
@@ -15,7 +15,10 @@ import org.mockito.Mock;
 import com.project.openmarket.domain.order.entity.Amount;
 import com.project.openmarket.domain.order.entity.eums.OrderStatus;
 import com.project.openmarket.domain.order.service.OrderService;
+import com.project.openmarket.domain.product.entity.Product;
 import com.project.openmarket.domain.product.service.ProductService;
+import com.project.openmarket.domain.user.entity.Consumer;
+import com.project.openmarket.domain.user.entity.Seller;
 import com.project.openmarket.domain.user.service.ConsumerService;
 import com.project.openmarket.domain.user.service.SellerService;
 import com.project.openmarket.global.exception.CustomException;
@@ -33,10 +36,9 @@ class OrderServiceTests extends ServiceTestMock {
 	@Mock
 	private ProductService productService;
 
-
+	@DisplayName("주문 확정 시 고객 포인트와 판매자 캐시가 적립된다.")
 	@Test
-	@DisplayName("주문 확정 시 고객의 포인트와 판매자의 캐시가 증가한다.")
-	void consumerGetPoint(){
+	void whenConfirmOrder_thenAddPointAndCash() {
 		Long amount = 10000L;
 		given(order.getTotalAmount()).willReturn(amount);
 
@@ -46,63 +48,58 @@ class OrderServiceTests extends ServiceTestMock {
 		assertThatNoException()
 			.isThrownBy(() -> orderService.processConfirmedOrder(order, seller, consumer));
 
-		then(consumerService)
-			.should(times(1))
-			.processPoints(expectedPoint, consumer);
-
-		then(sellerService)
-			.should(times(1))
-			.processPayment(expectedRevenue, seller);
+		verify(consumerService, times(1)).processPoints(expectedPoint, any(Consumer.class));
+		verify(sellerService, times(1)).processPayment(expectedRevenue, any(Seller.class));
 	}
 
 	@Nested
 	@DisplayName("주문 조회 시 ")
-	class findOrder{
-		@Test
+	class findOrder {
 		@DisplayName("유효한 주문 id를 가지고 주문을 조회하면 성공한다.")
-		void findByValidOrderId(){
+		@Test
+		void whenValidOrderId_thenReturnOrder() {
 			given(orderRepository.findById(any())).willReturn(Optional.of(order));
 			assertThatNoException()
 				.isThrownBy(() -> orderService.getOrderById("aaa"));
 
 		}
 
-		@Test
 		@DisplayName("유효하지 않은 주문 id를 가지고 주문을 조회하면 오류가 발생한다..")
-		void findByInvalidOrderId(){
+		@Test
+		void whenInvalidOrderId_thenThrowException() {
 			given(orderRepository.findById(any())).willReturn(Optional.empty());
 
 			assertThatThrownBy(() -> orderService.getOrderById("aaa"))
 				.isInstanceOf(CustomException.class)
 				.hasMessage(NOT_FOUND_ORDER.getMessage());
 
-
 		}
 	}
 
+	@DisplayName("주문 취소 시 재고와 소지금이 함께 복구된다.")
 	@Test
-	@DisplayName("구매 취소 메서드를 호출하면 상품의 재고를 증가시키는 메서드와 고객의 소지금을 증가시키는 메서드가 함께 호출된다.")
-	void processOrderCancelTest() {
+	void whenCancelOrder_thenRestockProductAndRefundConsumer() {
 		int count = 1;
-		Amount amount = new Amount(1000L,0L);
+		Amount amount = new Amount(1000L, 0L);
 		given(order.getCount()).willReturn(count);
 		given(order.getAmount()).willReturn(amount);
 
 		assertThatNoException()
 			.isThrownBy(() -> orderService.processOrderCancel(order, product, consumer));
 
-		then(productService)
-			.should(times(1))
-			.increaseProductStock(count, product);
-
-		then(consumerService)
-			.should(times(1))
-			.increaseAmount(amount, consumer);
-
-		then(order)
-			.should(times(1))
-			.updateOrderStatus(any(OrderStatus.class));
-
+		verify(productService, times(1)).increaseProductStock(anyInt(), any(Product.class));
+		verify(consumerService, times(1)).increaseAmount(any(Amount.class), any(Consumer.class));
+		verify(order, times(1)).updateOrderStatus(any(OrderStatus.class));
 	}
 
+	@DisplayName("주문을 리뷰 처리하면 isReviewed 필드가 true로 업데이트된다.")
+	@Test
+	void markAsReviewed_updatesFlag() {
+		//given
+		given(order.getId()).willReturn("67efeee0a7d6d52024e6ac6a");
+		//when
+		orderService.markAsReviewed(order);
+		//then
+		verify(orderRepository, times(1)).updateIsReviewed(anyString());
+	}
 }
