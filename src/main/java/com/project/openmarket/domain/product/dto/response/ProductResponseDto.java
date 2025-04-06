c533229d246518dbb3a939d3970bf5d4f5dfc908
원지윤 <waga1009@gmail.com>
2025-04-06 22:55:50 +0900

    feat: product detail조회 시 descrption까지 함께보내도록 추가

--- src/main/java/com/project/openmarket/domain/product/dto/response/ProductResponseDto.java
+++ src/main/java/com/project/openmarket/domain/product/dto/response/ProductResponseDto.java
@@ -3,9 +3,11 @@ package com.project.openmarket.domain.product.dto.response;
 import com.project.openmarket.domain.product.entity.Product;
 import com.project.openmarket.domain.user.dto.reposne.SellerResponseDto;
 
-public record ProductResponseDto(String id, String name, int price, double avgRating, SellerResponseDto seller) {
+public record ProductResponseDto(String id, String name, String description, int price, double avgRating,
+								 SellerResponseDto seller) {
 	public static ProductResponseDto of(Product product) {
-		return new ProductResponseDto(product.getId().toHexString(), product.getName(), product.getPrice(),
+		return new ProductResponseDto(product.getId().toHexString(), product.getName(), product.getDescription(),
+			product.getPrice(),
 			product.getAvgRating(), SellerResponseDto.of(product.getSeller()));
 	}
 }
